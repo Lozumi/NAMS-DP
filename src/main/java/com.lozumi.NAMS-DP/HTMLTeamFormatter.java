89bdd65a3a74457a0e35f62badc4f0d88c0b5f08
Lozumi Everkine <fantasynico1026@gmail.com>
2023-11-11 13:57:33 +0800

    Init: Initial the project.

---
+++ src/main/java/com.lozumi.NAMS-DP/HTMLTeamFormatter.java
@@ -0,0 +1,4 @@
+package com.lozumi.NAMS-DP
+
+class HTMLTeamFormatter {
+}
--- src/main/java/com.lozumi.NAMS-DP/UserSubSystem.java
+++ src/main/java/com.lozumi.NAMS-DP/UserSubSystem.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+package com.lozumi.NAMS-DP;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
